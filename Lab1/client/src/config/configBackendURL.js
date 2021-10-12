dae2684e8468786c0bffd4e1a4cb8f957a9abd03
Saurabh <svkannawar5@gmail.com>
2021-10-11 19:54:58 -0700

    Config changes

--- Lab1/client/src/config/configBackendURL.js
+++ Lab1/client/src/config/configBackendURL.js
@@ -1,4 +1,6 @@
-const BACKEND_URL = "http://localhost:5000"
+const BACKEND_URL = "http://3.144.84.184:5000"
+
+//"http://localhost:5000"
 const bearer= localStorage.getItem("accessToken");
 // eslint-disable-next-line import/no-anonymous-default-export
 export default BACKEND_URL
